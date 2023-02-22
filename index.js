f407d4afb99427c380c68e2f3f905e3cda9f803f
SHIVENDRA8004 <jat13632@gmail.com>
2023-02-22 21:14:59 +0530

    Created env file and configured it

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const colors = require("colors");
-
+// Configuring env file
+dotenv.config();
 // Rest Object
 const app = express();
 // Third Party Middlewares
@@ -16,7 +17,7 @@ app.get("/", (req, res) => {
 });
 
 // Port Variable
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 // Listening
 app.listen(PORT, () => {
   console.log(`Server Started Listening on Port ${PORT}`);
