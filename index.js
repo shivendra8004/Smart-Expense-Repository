13988ab08977f6d96a426486e237ed55d7d57134
SHIVENDRA8004 <jat13632@gmail.com>
2023-02-22 21:11:49 +0530

    Wrote code of basic server

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const colors = require("colors");
+
+// Rest Object
 const app = express();
+// Third Party Middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+// Routes and Middlewares
 app.get("/", (req, res) => {
   res.send("Working Properly");
 });
-app.listen("3000", () => {
-  console.log("Listening on Port 3000");
+
+// Port Variable
+const PORT = 3000 || process.env.PORT;
+// Listening
+app.listen(PORT, () => {
+  console.log(`Server Started Listening on Port ${PORT}`);
 });
