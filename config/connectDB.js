50bea9e4b0bb0f3a6c17598afbbfea17a5f5a213
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-01 00:24:40 +0530

    Database connected successfully

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
+const dotenv = require("dotenv");
+dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DATABASE_URL);
