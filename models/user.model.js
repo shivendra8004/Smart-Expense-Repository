a30f7f7c272d2cfb7d1452214137b4e9f967dd26
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-02 23:32:50 +0530

    Schema creation for user is done

--- models/user.model.js
+++ models/user.model.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
 const schema = mongoose.Schema();
-const userSchema = new schema({
-  name: {
-    type: String,
-    required: [true, "Name is Required"],
+const userSchema = new schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is Required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is Required and Must be unique"],
+      unique: true,
+    },
+    password: {
+      type: password,
+      required: [true, "Password is Required"],
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Email is Required and Must be unique"],
-    unique: true,
-  },
-  password: {
-    type: password,
-    required: [true, "Password is Required"],
-  },
-});
+  { timestamps: true }
+);
 
 const userModel = mongoose.model("userModel", userSchema);
 module.exports = userModel;
