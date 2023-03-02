8dec10f6832577725713d6de539e34959245bc0e
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-02 23:31:32 +0530

    Model is created

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,20 @@
 const mongoose = require("mongoose");
 const schema = mongoose.Schema();
-const userSchema = new schema({});
+const userSchema = new schema({
+  name: {
+    type: String,
+    required: [true, "Name is Required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is Required and Must be unique"],
+    unique: true,
+  },
+  password: {
+    type: password,
+    required: [true, "Password is Required"],
+  },
+});
 
 const userModel = mongoose.model("userModel", userSchema);
 module.exports = userModel;
