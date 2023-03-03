e8c4a6ea9e02b9b4e6131e99bee00468e678513e
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-03 17:28:51 +0530

    Login Controller created

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,7 +2,12 @@ const userModel = require("../models/user.model");
 // Controllers
 const loginController = async (req, res) => {
   try {
-    res.send("Login");
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email, password });
+    if (!user) {
+      return res.status(404).send("User Not Found");
+    }
+    res.status(200).json(user);
   } catch (error) {
     res.status(400).json({
       success: false,
