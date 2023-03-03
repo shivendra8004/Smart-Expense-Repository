3604d71965a8e61a80f2ba35434ca3caf70fea8f
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-03 17:01:36 +0530

    Created basic controller for user

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const userController = require("../controllers/user.controller");
 const router = express.Router();
 
-router.post("/login");
+// Post Router
+router.post("/login", userController);
 
 module.exports = router;
