5ead3a94705f13ef4e0ae2143ac0453ea8676dec
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-04 23:51:23 +0530

    Changing some styling

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-dark text-light">
+    <div className="bg-dark text-light p-3">
       <h5 className="text-center">All rights reserved &copy; Smart Expense</h5>
     </div>
   );
