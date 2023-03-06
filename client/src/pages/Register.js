6f0246267415f52451ec1bd27fe168e0a34fc1fe
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-06 22:55:55 +0530

    Submit handler working correctly

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,7 +2,10 @@ import React from "react";
 import { Form, Input } from "antd";
 import { Link } from "react-router-dom";
 const Register = () => {
-  const handleSubmit = () => {};
+  // Form Submit
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
   return (
     <>
       <div className="register">
