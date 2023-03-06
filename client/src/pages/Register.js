1ce1e4459a97d0294a0a8e20817afbc4662d5f12
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-06 22:49:07 +0530

    Basic register page done

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { Form, Input } from "antd";
+import { Link } from "react-router-dom";
 const Register = () => {
   return (
     <>
       <div className="register">
         <Form layout="vertical">
-          <Form.Item label="Enter Name" name="name">
+          <h2>Register User </h2>
+          <Form.Item label="Name" name="name">
             <Input />
           </Form.Item>
+          <Form.Item label="Email" name="email">
+            <Input type="email" />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" />
+          </Form.Item>
+          <div className="d-flex justify-content-between">
+            <Link to="/login">Already Registered? Login</Link>
+          </div>
+          <button className="btn btn-primary">Register</button>
         </Form>
       </div>
     </>
