0e71be734c66c0f3f141fe5b47ae818b29b1b6f4
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-06 22:22:16 +0530

    Successfully added andt in project

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "antd/dist/antd.min.css";
+import "antd/dist/reset.css";
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
