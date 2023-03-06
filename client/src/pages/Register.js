7d00c13c73c548dc46946bcca977b8c65a6fa4bc
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-06 22:09:19 +0530

    Creting pages for login and register

---
+++ client/src/pages/Register.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <>
+      <div className="d-flex align-items-center justify-content-center">
+        <h1>Login Here</h1>
+      </div>
+    </>
+  );
+};
+
+export default Register;
