a1ce2f2d51d6a333abe5ffc341fd63c4b340e2b9
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-06 22:58:35 +0530

    Creating login page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,30 @@
+import { Form, Input } from "antd";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <>
+      <div className="login d-flex align-items-center justify-content-center">
+        <Form layout="vertical" onFinish={handleSubmit}>
+          <h2>Register User </h2>
+          <Form.Item label="Name" name="name">
+            <Input />
+          </Form.Item>
+          <Form.Item label="Email" name="email">
+            <Input type="email" />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" />
+          </Form.Item>
+          <div className="d-flex justify-content-between">
+            <Link to="/login">Already Registered? Login</Link>
+            <button className="btn btn-primary">Register</button>
+          </div>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default Login;
