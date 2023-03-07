bfae4e762e2fcf62e6a8d6b3d123e4a23afac97f
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-07 22:12:38 +0530

    Default response working

--- routes/main.route.js
+++ routes/main.route.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = require("./user.route");
 const mainRouter = express.Router();
-
+mainRouter.get("/", (req, res) => {
+  res.send("Default Response");
+});
 mainRouter.use("/user", router);
 module.exports = mainRouter;
