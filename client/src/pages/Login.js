a04cbe9d89f30917803031ce550caeaaf79bfff5
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-07 23:03:09 +0530

    Added spinner on login page also

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,6 +2,7 @@ import { Form, Input, message } from "antd";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import Spinner from "../components/Spinner";
 const Login = () => {
   // To use Navigate
   const navigate = useNavigate();
@@ -10,14 +11,19 @@ const Login = () => {
   // To handle submit
   const handleSubmit = async (values) => {
     try {
-      await 
+      setLoading(true);
+      await axios.post("/users/login", values);
+      setLoading(false);
+      message.success("Login Suceessful");
     } catch (error) {
+      setLoading(false);
       message.error("Invalid Username or Password");
     }
   };
   return (
     <>
       <div className="login d-flex align-items-center justify-content-center">
+        {loading && <Spinner />}
         <Form layout="vertical" onFinish={handleSubmit}>
           <h2>Login</h2>
           <Form.Item label="Email" name="email">
