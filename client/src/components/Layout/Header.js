09a3084124aef1dd81833a55fbb4f676ca4f09c7
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-08 19:52:15 +0530

    Creating logout button

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [loginUser, setLoginUser] = useState("");
@@ -28,14 +29,14 @@ const Header = () => {
             Smart Expense
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <p className="nav-link active" aria-current="page">
+                <Link className="nav-link active" aria-current="page" to="/user">
                   {loginUser && loginUser.name}
-                </p>
+                </Link>
               </li>
               <li className="nav-item">
-                <p className="nav-link active" aria-current="page">
+                <Link className="nav-link active" aria-current="page" to="/user">
                   Logout
-                </p>
+                </Link>
               </li>
             </ul>
           </div>
