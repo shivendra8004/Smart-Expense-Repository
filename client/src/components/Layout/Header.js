f78a13bf50d97b1d36afb5ab9ef34afacd7660c8
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-08 20:01:32 +0530

    Added success alert message using antd

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,3 +1,4 @@
+import { message } from "antd";
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
@@ -14,6 +15,7 @@ const Header = () => {
   const logOutHandler = () => {
     localStorage.removeItem("user");
     navigate("/login");
+    message.success("Logout Successfully");
   };
   return (
     <>
