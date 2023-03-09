e41a4eefbefd85e14e79fe3b615ec7b50623d39b
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-09 22:41:22 +0530

    Created route for transactions in main router

--- routes/main.route.js
+++ routes/main.route.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const userRouter = require("./user.route");
+const transactionRouter = require("./transaction.route");
 const mainRouter = express.Router();
 mainRouter.get("/", (req, res) => {
   res.send("Default Response");
 });
 mainRouter.use("/users", userRouter);
+mainRouter.use("/transactions", transactionRouter);
 module.exports = mainRouter;
