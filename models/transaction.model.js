ba2d164d67039e6d21b7cae3a6e0e4d4be0a0602
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-09 19:55:35 +0530

    Created Schema for transaction model

--- models/transaction.model.js
+++ models/transaction.model.js
@@ -1,9 +1,29 @@
 const mongoose = require("mongoose");
-const transactionSchema = new mongoose.Schema({
-  amount: {
-    type: String,
-    required: [true, "Amount is Required"],
+const transactionSchema = new mongoose.Schema(
+  {
+    amount: {
+      type: Number,
+      required: [true, "Amount is Required"],
+    },
+    category: {
+      type: String,
+      required: [true, "Category is Required"],
+    },
+    reference: {
+      type: String,
+      required: [true, "Reference is Required"],
+    },
+    description: {
+      type: String,
+    },
+    date: {
+      type: String,
+      required: [true, "Date is Required"],
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 const transactionModel = mongoose.model("transections", transactionSchema);
 module.exports = transactionModel;
