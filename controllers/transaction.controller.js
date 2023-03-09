66abb969cc2065cf1673b5cf8366edbac0a396b4
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-09 20:08:30 +0530

    Created a dummy trnsaction controller

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -1,1 +1,11 @@
 const transactionModel = require("../models/transaction.model");
+const getAllTransactions = (req, res) => {
+  res.send("Getting All Transactions");
+};
+const AddTransactions = (req, res) => {
+  res.send("Add Transaction");
+};
+module.exports = {
+  getAllTransactions,
+  AddTransactions,
+};
