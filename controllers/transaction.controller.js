f55f6a9367834b7aa64a4670397b11671d1e826e
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-09 22:46:53 +0530

    Controller created for add transaction

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -2,8 +2,12 @@ const transactionModel = require("../models/transaction.model");
 const getAllTransactions = (req, res) => {
   res.send("Getting All Transactions");
 };
-const addTransactions = (req, res) => {
-  res.send("Add Transaction");
+const addTransactions = async (req, res) => {
+  try {
+    const newTransaction = new transactionModel(req.body);
+    await newTransaction.save();
+    res.status(201).send("Transaction Created Successfully");
+  } catch (error) {}
 };
 module.exports = {
   getAllTransactions,
