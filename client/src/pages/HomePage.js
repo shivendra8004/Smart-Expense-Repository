c5336b2fb938db1a2ec70eb1cc9422d1420d0060
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-10 22:35:30 +0530

    Show Modal

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { Modal } from "antd";
 import Layout from "../components/Layout/Layout";
 
 const HomePage = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <Layout>
       <div className="filters">
         <div>Range Filters</div>
         <div>
-          <button className="btn btn-primary">Add Transaction</button>
+          <button className="btn btn-primary" onClick={() => setShowModal(true)}>
+            Add Transaction
+          </button>
         </div>
       </div>
       <div className="content"></div>
+      <Modal title="Add Transaction" visible={showModal}></Modal>
     </Layout>
   );
 };
