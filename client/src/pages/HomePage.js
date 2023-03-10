019be60bd12efbb655683f873ddf3bdb67cd7b5e
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-10 23:21:59 +0530

    Successfully created and tested form

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Input, Modal } from "antd";
+import { Form, Input, Modal, Select } from "antd";
 import Layout from "../components/Layout/Layout";
 
 const HomePage = () => {
@@ -24,17 +24,35 @@ const HomePage = () => {
           <Form.Item label="Amount" name="amount">
             <Input type="text" required="True" />
           </Form.Item>
+          <Form.Item label="Type" name="type">
+            <Select>
+              <Select.Option value="income">Income</Select.Option>
+              <Select.Option value="expense">Expense</Select.Option>
+            </Select>
+          </Form.Item>
           <Form.Item label="Category" name="category">
-            <Input type="text" required="True" />
+            <Select>
+              <Select.Option value="salary">Salary</Select.Option>
+              <Select.Option value="tip">Tip</Select.Option>
+              <Select.Option value="freelance">Free Lancing</Select.Option>
+              <Select.Option value="other-income">Other Income</Select.Option>
+              <Select.Option value="grocery">Grocery</Select.Option>
+              <Select.Option value="travel">Travel</Select.Option>
+              <Select.Option value="medical">Medical</Select.Option>
+              <Select.Option value="food">Food</Select.Option>
+              <Select.Option value="fee">Fee</Select.Option>
+              <Select.Option value="tax">Tax</Select.Option>
+              <Select.Option value="other">Other</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="Date" name="date">
+            <Input type="date" required="True" />
           </Form.Item>
           <Form.Item label="Reference" name="reference">
             <Input type="text" required="True" />
           </Form.Item>
           <Form.Item label="Description" name="description">
             <Input type="text" required="False" />
-          </Form.Item>
-          <Form.Item label="Date" name="date">
-            <Input type="date" required="True" />
           </Form.Item>
           <button className="btn btn-primary">Submit</button>
         </Form>
