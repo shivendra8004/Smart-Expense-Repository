6624dbade26951cdcab7836d8181dae5eb8bc7dc
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-10 22:28:46 +0530

    Styling Filters tab

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -4,7 +4,12 @@ import Layout from "../components/Layout/Layout";
 const HomePage = () => {
   return (
     <Layout>
-      <div className="filters"></div>
+      <div className="filters">
+        <div>Range Filters</div>
+        <div>
+          <button className="btn btn-primary">Add Transaction</button>
+        </div>
+      </div>
       <div className="content"></div>
     </Layout>
   );
