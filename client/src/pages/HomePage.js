89267971aef2fd94440d4f44ae8956820ffc89fb
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-10 22:23:43 +0530

    Creating UI for transaction model

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -4,8 +4,8 @@ import Layout from "../components/Layout/Layout";
 const HomePage = () => {
   return (
     <Layout>
-      <h1>Home Page</h1>
-      <p>Enter all your expenses</p>
+      <div className="filters"></div>
+      <div className="content"></div>
     </Layout>
   );
 };
