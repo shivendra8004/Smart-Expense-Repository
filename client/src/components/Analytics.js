a4debd7728118bd6dc83acebd4b42ba6d7c0e3e7
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-16 15:30:02 +0530

    Created progress bar for expense also

--- client/src/components/Analytics.js
+++ client/src/components/Analytics.js
@@ -4,8 +4,8 @@ const Analytics = ({ allTransaction }) => {
   const totalTransaction = allTransaction.length;
   const totalIncomeTransaction = allTransaction.filter((transaction) => transaction.type === "income");
   const totalExpenseTransaction = allTransaction.filter((transaction) => transaction.type === "expense");
-  const totalIncomePercentage = (totalIncomeTransaction / totalTransaction) * 100;
-  const totalExpensePercentage = (totalExpenseTransaction / totalTransaction) * 100;
+  const totalIncomePercentage = (totalIncomeTransaction.length / totalTransaction) * 100;
+  const totalExpensePercentage = (totalExpenseTransaction.length / totalTransaction) * 100;
   return (
     <div>
       <div className="row m-3">
@@ -15,13 +15,23 @@ const Analytics = ({ allTransaction }) => {
             <div className="card-body">
               <h5>Total Income : {totalIncomeTransaction.length}</h5>
               <h5>Total Expense : {totalExpenseTransaction.length}</h5>
-              <div>
-                <Progress
-                  type="circle"
-                  strokeColor={"green"}
-                  className="mx-2"
-                  percent={totalIncomePercentage.toFixed(0)}
-                />
+              <div className="d-flex">
+                <div>
+                  <Progress
+                    type="circle"
+                    strokeColor={"green"}
+                    className="mx-2"
+                    percent={totalIncomePercentage.toFixed(0)}
+                  />
+                </div>
+                <div>
+                  <Progress
+                    type="circle"
+                    strokeColor={"red"}
+                    className="mx-2"
+                    percent={totalExpensePercentage.toFixed(0)}
+                  />
+                </div>
               </div>
             </div>
           </div>
