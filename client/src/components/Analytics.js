78a32529a984f76087f239947966fcf99c607b87
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-16 15:13:36 +0530

    Created all variables to display them in chart formate

--- client/src/components/Analytics.js
+++ client/src/components/Analytics.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 const Analytics = ({ allTransaction }) => {
+  const totalTransaction = allTransaction.length;
+  const totalIncomeTransaction = allTransaction.filter((transaction) => transaction.type === "income");
+  const totalExpenseTransaction = allTransaction.filter((transaction) => transaction.type === "expense");
+  const totalIncomePercentage = (totalIncomeTransaction / totalTransaction) * 100;
+  const totalExpensePercentage = (totalExpenseTransaction / totalTransaction) * 100;
   return (
     <div>
       <h1>Analytics</h1>
