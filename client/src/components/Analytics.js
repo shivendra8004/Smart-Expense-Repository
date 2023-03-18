d089b947da22b6178d6d2a2b8d08e1357ab61a2e
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-18 16:01:00 +0530

    Created a progress bar for each category

--- client/src/components/Analytics.js
+++ client/src/components/Analytics.js
@@ -104,6 +104,7 @@ const Analytics = ({ allTransaction }) => {
               <div className="card">
                 <div className="card-body">
                   <h5>{categories}</h5>
+                  <Progress percent={(amount / totalIncomeTurnover) * 100} />
                 </div>
               </div>
             );
